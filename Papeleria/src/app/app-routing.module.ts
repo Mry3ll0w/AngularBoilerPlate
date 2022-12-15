458d9de5ffd383687d84b12dfd8b1962f6610a03
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2022-12-15 20:48:00 +0100

    listo routing

--- Papeleria/src/app/app-routing.module.ts
+++ Papeleria/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { ExampleComponent } from './example/example.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'cosas', component: ExampleComponent}
+  { path: 'cosas', component: ExampleComponent},
+  { path: '', component: HomeComponent}
 ];
 
 @NgModule({
--- Papeleria/src/app/app.module.ts
+++ Papeleria/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExampleComponent } from './example/example.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleComponent
+    ExampleComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
