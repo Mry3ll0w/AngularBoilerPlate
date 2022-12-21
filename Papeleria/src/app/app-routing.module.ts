34970c4ee10e9f6292a840d320b2fd9df66a4241
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2022-12-21 19:54:08 +0100

    listas las faq

--- Papeleria/src/app/app-routing.module.ts
+++ Papeleria/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ExampleComponent } from './example/example.component';
 import { FrecuentedQuestionsComponent } from './frecuented-questions/frecuented-questions.component';
 import { HomeComponent } from './home/home.component';
+import { LocationComponent } from './location/location.component';
 
 const routes: Routes = [
   { path: 'cosas', component: ExampleComponent},
   { path: '', component: HomeComponent},
-  { path: 'preguntasFrecuentes', component: FrecuentedQuestionsComponent}
+  { path: 'faq', component: FrecuentedQuestionsComponent},
+  { path: 'location', component: LocationComponent}
 ];
 
 @NgModule({
